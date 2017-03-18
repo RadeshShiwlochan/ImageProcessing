2b24c17a0855dce694c24582e98fd6d326f0cbe7
radeshshiwlochan <radesh0430@gmail.com>
2017-03-18 02:01:02 -0400

    adds executeChainCode in ChainCode class

--- Project6/main.cpp
+++ Project6/main.cpp
@@ -4,188 +4,207 @@ using namespace std;
 
 class Image {
 
-private:
-	int numRows;
-	int numCols;
-	int minVal;
-	int maxVal;
-	int** zeroFramedAry;
+	private:
+		int numRows;
+		int numCols;
+		int minVal;
+		int maxVal;
+		int** zeroFramedAry;
 
-public: 
-	Image(string);
-	~Image();
-	void zeroFramed();
-	void loadImage(string);
-	int getNumRows();
-	int getNumCols();
-	int getMinVal();
-	int getMaxVal();
+	public: 
+		Image(string);
+		~Image();
+		void zeroFramed();
+		void loadImage(string);
+		int getNumRows();
+		int getNumCols();
+		int getMinVal();
+		int getMaxVal();
+		int getIndexVal(int, int);
 
-	//temporary functions
-	void printImage();
-};
+		//temporary functions
+		void printImage();
+	};
 
-Image::Image(string inputFile) {
-	ifstream readFile;
-	readFile.open(inputFile);
-	readFile >> numRows >> numCols >>
-	            minVal >> maxVal;
-	int rowSize = numRows + 2;
-	int colSize = numCols + 2;
-	zeroFramedAry = new int*[rowSize];
-	for(int i = 0; i < rowSize; ++i)
-		zeroFramedAry[i] = new int[colSize](); 
+	Image::Image(string inputFile) {
+		ifstream readFile;
+		readFile.open(inputFile);
+		readFile >> numRows >> numCols >>
+		            minVal >> maxVal;
+		int rowSize = numRows + 2;
+		int colSize = numCols + 2;
+		zeroFramedAry = new int*[rowSize];
+		for(int i = 0; i < rowSize; ++i)
+			zeroFramedAry[i] = new int[colSize](); 
 
-	readFile.close();	           
-}
-
-Image::~Image() {
-	int rowSize = numRows + 2;
-	for(int i = 0; i < rowSize; ++i)
-		delete [] zeroFramedAry[i];
-	delete [] zeroFramedAry;
-}
-
-void Image::zeroFramed() {
-	for(int i = 0; i <= numRows +1; i++) {
-		zeroFramedAry[i][0]           = zeroFramedAry[i][1];
-		zeroFramedAry[i][numCols + 1] = zeroFramedAry[i][numCols];
+		readFile.close();	           
 	}
 
-	for(int j = 0; j <= numCols + 1; j++) {
-		zeroFramedAry[0][j]           = zeroFramedAry[1][j];
-		zeroFramedAry[numRows + 1][j] = zeroFramedAry[numRows][j];
+	Image::~Image() {
+		int rowSize = numRows + 2;
+		for(int i = 0; i < rowSize; ++i)
+			delete [] zeroFramedAry[i];
+		delete [] zeroFramedAry;
 	}
-}
 
-void Image::loadImage(string inputFile) {
-	ifstream readFile;
-	int pixelValue = -1;
-	int rowSize = numRows + 1;
-	int colSize = numCols + 1;
-	readFile.open(inputFile);
-	for(int i = 0; i < 4; ++i)
-		readFile >> pixelValue;
+	void Image::zeroFramed() {
+		for(int i = 0; i <= numRows +1; i++) {
+			zeroFramedAry[i][0]           = zeroFramedAry[i][1];
+			zeroFramedAry[i][numCols + 1] = zeroFramedAry[i][numCols];
+		}
 
-	for(int i = 1; i < rowSize; ++i) {
-		for(int j = 1; j < colSize; ++j) {
+		for(int j = 0; j <= numCols + 1; j++) {
+			zeroFramedAry[0][j]           = zeroFramedAry[1][j];
+			zeroFramedAry[numRows + 1][j] = zeroFramedAry[numRows][j];
+		}
+	}
+
+	void Image::loadImage(string inputFile) {
+		ifstream readFile;
+		int pixelValue = -1;
+		int rowSize = numRows + 1;
+		int colSize = numCols + 1;
+		readFile.open(inputFile);
+		for(int i = 0; i < 4; ++i)
 			readFile >> pixelValue;
-			zeroFramedAry[i][j] = pixelValue;
+
+		for(int i = 1; i < rowSize; ++i) {
+			for(int j = 1; j < colSize; ++j) {
+				readFile >> pixelValue;
+				zeroFramedAry[i][j] = pixelValue;
+			}
 		}
+		readFile.close();
 	}
-	readFile.close();
-}
 
-int Image::getNumRows() { return numRows; }
+	int Image::getNumRows() { return numRows; }
 
-int Image::getNumCols() { return numCols; }
+	int Image::getNumCols() { return numCols; }
 
-int Image::getMaxVal() { return maxVal; }
+	int Image::getMaxVal() { return maxVal; }
 
-int Image::getMinVal() { return minVal; }
+	int Image::getMinVal() { return minVal; }
 
-void Image::printImage() {
-	for(int i = 0; i < numRows + 2; ++i) {
-		for(int j = 0; j < numCols + 2; ++j) {
-			cout << zeroFramedAry[i][j] << " "; 
-		}
-		cout << endl;
-	 }
-}
+	void Image::printImage() {
+		for(int i = 0; i < numRows + 2; ++i) {
+			for(int j = 0; j < numCols + 2; ++j) {
+				cout << zeroFramedAry[i][j] << " "; 
+			}
+			cout << endl;
+		 }
+		 cout << endl;
+	}
+
+	int Image::getIndexVal(int row, int col) {
+		return zeroFramedAry[row][col];
+	}
 
 class Point {
 
-private:
-	int row;
-	int col;
+	private:
+		int row;
+		int col;
 
-public: 
-	Point();
-	Point(int, int);
-	int getPointRow();
-	int getPointCol();
-	void setPointRow(int);
-	void setPointCol(int);
-	void printPoint();	
-};
+	public: 
+		Point();
+		Point(int, int);
+		int getPointRow();
+		int getPointCol();
+		void setPointRow(int);
+		void setPointCol(int);
+		void printPoint();	
+	};
 
-Point::Point() {
-	row = 0;
-	col = 0;
-}
+	Point::Point() {
+		row = 0;
+		col = 0;
+	}
 
-Point::Point(int rowVal, int colVal) {
-	row = rowVal;
-	col = colVal;
-}
+	Point::Point(int rowVal, int colVal) {
+		row = rowVal;
+		col = colVal;
+	}
 
-int Point::getPointRow() { return row; }
+	int Point::getPointRow() { return row; }
 
-int Point::getPointCol() { return col; }
+	int Point::getPointCol() { return col; }
 
-void Point::setPointRow(int rowVal) { row = rowVal; }
+	void Point::setPointRow(int rowVal) { row = rowVal; }
 
-void Point::setPointCol(int colVal) { col = colVal; }
+	void Point::setPointCol(int colVal) { col = colVal; }
 
-void Point::printPoint() {
-	cout << "row " << row << " \n" 
-	<< " col " << col << endl;
-}
+	void Point::printPoint() {
+		cout << "row " << row << " \n" 
+		<< " col " << col << endl;
+	}
 
 class ChainCode {
 
-private: 
-	Point neighborCoord[8];
-	Point currentP;
-	Point nextP;
-	Point lastQ;
-	Point nextDirTable[8];
-	Point nextDir;
-	Point Pchain;
+	private: 
+		Point neighborCoord[8];
+		Point currentP;
+		Point nextP;
+		Point lastQ;
+		Point nextDirTable[8];
+		Point nextDir;
+		Point Pchain;
 
-public: 
-	ChainCode();
-	void loadNeighborCoord();
-	void findNextPoint();
-	void prettyPrint(string);
-};
+	public: 
+		ChainCode();
+		void loadNeighborCoord();
+		void findNextPoint();
+		void prettyPrint(string);
+		void executeChainCode(Image&);
+	};
 
-ChainCode::ChainCode() {
+	ChainCode::ChainCode() {
 
-	for(int i = 0; i < 8; ++i) {
-		neighborCoord[i].setPointRow(0);
-		neighborCoord[i].setPointCol(0);
-		nextDirTable[i].setPointRow (0);
-		nextDirTable[i].setPointCol(0);
+		for(int i = 0; i < 8; ++i) {
+			neighborCoord[i].setPointRow(0);
+			neighborCoord[i].setPointCol(0);
+			nextDirTable[i].setPointRow (0);
+			nextDirTable[i].setPointCol(0);
+		}
 	}
-}
 
-void ChainCode::loadNeighborCoord() {
+	void ChainCode::loadNeighborCoord() {
 
-}
+	}
 
-void ChainCode::findNextPoint() {
+	void ChainCode::findNextPoint() {
 
-}
+	}
 
-void ChainCode::prettyPrint(string outputFile) {
+	void ChainCode::prettyPrint(string outputFile) {
 
-	// ofstream printToFile;
-	// printToFile.open(outputFile);
-	// int pixel_value = -1;
-	// 	printToFile << "Input Image Pretty Print" << endl;
-	// 	printToFile << endl;
-	// 	for(int i = 1; i <= numRows + 1; i++) {
-	// 		for(int j = 1; j <= numCols + 1; j++) {
-	// 			//pixel_value = firstAry[i][j];
-	// 			if(pixel_value == 1) 
-	// 				printToFile << pixel_value << " ";
-	// 			else 
-	// 			    printToFile << "  ";
-	// 		}
-	// 		printToFile << endl;
-	// 	}
-}//prettyPrint method
+		// ofstream printToFile;
+		// printToFile.open(outputFile);
+		// int pixel_value = -1;
+		// 	printToFile << "Input Image Pretty Print" << endl;
+		// 	printToFile << endl;
+		// 	for(int i = 1; i <= numRows + 1; i++) {
+		// 		for(int j = 1; j <= numCols + 1; j++) {
+		// 			//pixel_value = firstAry[i][j];
+		// 			if(pixel_value == 1) 
+		// 				printToFile << pixel_value << " ";
+		// 			else 
+		// 			    printToFile << "  ";
+		// 		}
+		// 		printToFile << endl;
+		// 	}
+	}//prettyPrint method
+
+	void ChainCode::executeChainCode(Image& image) {
+		 int rowSize = image.getNumRows();
+		 int colSize = image.getNumCols();
+		for(int i = 1; i < rowSize; ++i) {
+			for(int j = 1; j < colSize; ++j) {
+				if(image.getIndexVal(i,j) > 0) 
+					cout << image.getIndexVal(i,j) << " ";
+			}
+			cout << endl;
+		}
+	}
 
 int main(int argc, char* argv []) {
 	if(argc != 4) {
@@ -203,6 +222,8 @@ int main(int argc, char* argv []) {
 	image.zeroFramed();
 	image.loadImage(inputFile);
 	image.printImage();
+	ChainCode chainCode;
+	chainCode.executeChainCode(image);
 }
 
 
