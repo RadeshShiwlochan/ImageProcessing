2f9e7b22250ecbbfe9ce3decd2baf075fec20bb6
radeshshiwlochan <radesh0430@gmail.com>
2017-03-26 17:14:02 -0400

    initializes the mask arrays

--- Project8/main.cpp
+++ Project8/main.cpp
@@ -110,9 +110,81 @@ class Edge {
 		int** GradiantEdge;
 
 	public:
+		Edge();
 		void convolute(int,int); //needs mask as an argument
+		void initalizeVertMask();
+		void initalizeHortMask();
+		void initalizeRightMask();
+		void initalizeLeftMask();
 		void computeGradient(int,int);	
+
 };
+
+Edge::Edge() {
+	initalizeVertMask();
+	initalizeHortMask();
+	initalizeRightMask();
+	initalizeLeftMask();
+	
+}
+	
+	
+
+	
+
+	void Edge::initalizeVertMask() {
+		maskHorizontal[0][0] = 1;
+		maskHorizontal[0][1] = 2;
+		maskHorizontal[0][2] = 1;
+		maskHorizontal[1][0] = 0;
+		maskHorizontal[1][1] = 0;
+		maskHorizontal[1][2] = 0;
+		maskHorizontal[2][0] = -1;
+		maskHorizontal[2][1] = -2;
+		maskHorizontal[2][2] = -1;
+	
+	}
+
+	void Edge::initalizeHortMask() {
+
+		maskVertical[0][0] = 1;
+		maskVertical[0][1] = 0;
+		maskVertical[0][2] = -1;
+		maskVertical[1][0] = 2;
+		maskVertical[1][1] = 0;
+		maskVertical[1][2] = -2;
+		maskVertical[2][0] = 1;
+		maskVertical[2][1] = 0;
+		maskVertical[2][2] = -1;
+		
+	}
+
+	void Edge::initalizeRightMask() {
+		//right diagonal
+		maskRightDiag[0][0] = 0;
+		maskRightDiag[0][1] = 1;
+		maskRightDiag[0][2] = 2;
+		maskRightDiag[1][1] = 0;
+		maskRightDiag[1][2] = 1;
+		maskRightDiag[2][0] = -2;
+		maskRightDiag[2][1] = -1;
+		maskRightDiag[2][2] = 0;
+		
+	}
+
+	void Edge::initalizeLeftMask() {
+		//left diagonal
+		maskLeftDiag[0][0] = 2;
+		maskLeftDiag[0][1] = 1;
+		maskLeftDiag[0][2] = 0;
+		maskLeftDiag[1][0] = 1;
+		maskLeftDiag[1][1] = 0;
+		maskLeftDiag[1][2] = -1;
+		maskLeftDiag[2][0] = 0;
+		maskLeftDiag[2][1] = -1;
+		maskLeftDiag[2][2] = -2;
+		
+	}
 
 int main(int argc, char* argv[]) {
 	string inputFile = argv[1];
