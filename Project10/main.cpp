805f0ef8d0a37883d1b3d02c2e7b7ae9fd4a2eb3
radeshshiwlochan <radesh0430@gmail.com>
2017-05-03 00:26:09 -0400

    creates new input in project 4

--- Project10/main.cpp
+++ Project10/main.cpp
@@ -16,8 +16,9 @@ private:
 
 public:
 	Expansion(string, string);
+	~Expansion();
 	void zeroFramed();
-	void loadImage();
+	void loadImage(string,string);
 	void copyAry();
 	void doExpansion();
 	void northExpansion();
@@ -25,10 +26,13 @@ public:
 	void westExpansion();
 	void eastExpansion();
 	void prettyPrint();	
+	//temporary functionss
+	void printImage(string, string);
 
 };
 
 Expansion::Expansion(string input1, string input2) {
+
 	ifstream readFile1;
 	ifstream readFile2;
 	readFile1.open(input1);
@@ -50,7 +54,93 @@ Expansion::Expansion(string input1, string input2) {
 	readFile2.close();
 }
 
+Expansion::~Expansion() {
+
+	for(int i = 0; i < numRows + 2; ++i) {
+		delete [] objectAry[i];
+		delete [] firstAry[i];
+		delete [] secondAry[i];
+	}
+	delete [] objectAry;
+	delete [] firstAry;
+	delete [] secondAry;
+}
+
+void Expansion::zeroFramed() {
+	for(int i = 0; i <= numRows +1; i++) {
+		objectAry[i][0]           = 0;
+		objectAry[i][numCols + 1] = 0;
+		firstAry[i][0]            = 0;
+		firstAry[i][numCols + 1]  = 0;
+		secondAry[i][0]           = 0;
+		secondAry[i][numCols + 1] = 0;
+	}
+
+	for(int j = 0; j <= numCols + 1; j++) {
+		 objectAry[0][j]           = 0;
+		 objectAry[numRows + 1][j] = 0;
+		 firstAry[0][j]            = 0;
+		 firstAry[numRows + 1]     = 0;
+		 secondAry[0][j]           = 0;
+		 secondAry[numRows + 1]    = 0;
+	}
+}
+
+void Expansion::loadImage(string inputFile1, string inputFile2) {
+
+	ifstream readFile1;
+	ifstream readFile2;
+	readFile1.open(input1);
+	readFile2.open(input2);
+	int numFromFile = -9999;
+	//skip the header
+	readFile1 >> numRows >> numCols >> minVal >> maxVal;
+	for(int i = 1; i < numRows + 2; ++i) {
+		for(int j = 1; j < numCols + 2; ++j) {
+			readFile1 >> numFromFile;
+			objectAry[i][j] = numFromFile;
+	    }
+	}
+	readFile2 >> numRows >> numCols >> minVal >> maxVal;
+	for(int i = 1; i < numRows + 2; ++i) {
+		for(int j = 1; j < numCols + 2; ++j) {
+			readFile2 >> numFromFile;
+			firstAry[i][j] = numFromFile;
+		}
+	}
+	readFile1.close();
+	readFile2.close();
+}
+
+void Expansion::copyAry() {
+	for(int i = 0; i < numRows + 2; ++i) {
+		for(int j = 0; j < numCols + 2; ++j) {
+			firstAry[i][j] = secondAry[i][j];
+		}
+	}
+}
+
+void Expansion::printImage(string output1, string output2) {
+	
+	ofstream printToFile1;
+	ofstream printToFile2;
+	printToFile1.open(output1);
+	printToFile2.open(output2);
+	for(int i = 0 i < numRows + 2; ++i) {
+		for(int j = 0; j < numCols + 2; ++j) {
+			printToFile1 << objectAry[i][j] << " ";
+			printToFile2 << firstAry[i][j] << " ";
+		}
+		printToFile1 << endl;
+		printToFile2 << endl;
+	}
+	printToFile1.close();
+	printToFile2.close();
+
+}
+
 int main(int argc, char* argv[]) {
+
 	if(argc != 5) {
 		cout << "Program needs 4 files:\n" <<
 		"Two input files and two outputfiles" << 
