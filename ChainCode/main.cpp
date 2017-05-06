a9aa03f4b61d4d284f5845fe7d4e28e79f07e606
radeshshiwlochan <radesh0430@gmail.com>
2017-05-06 00:47:31 -0400

    adds runAlgorithm for chainCode

--- ChainCode/main.cpp
+++ ChainCode/main.cpp
@@ -146,6 +146,92 @@ ChainCode::~ChainCode() {
 		delete [] neighborCoord;
 }
 
+void ChainCode::loadNeighborCoord() {
+	neighborCoord[0].row = currentP.row;
+	neighborCoord[0].col = currentP.col + 1;
+	neighborCoord[1].row = currentP.row - 1; 
+	neighborCoord[1].col = currentP.col + 1;
+	neighborCoord[2].row = currentP.row - 1;
+	neighborCoord[2].col = currentP.col;
+	neighborCoord[3].row = currentP.row - 1;
+	neighborCoord[3].col = currentP.col - 1;
+	neighborCoord[4].row = currentP.row;
+	neighborCoord[4].col = currentP.col - 1;
+	neighborCoord[5].row = currentP.row + 1;
+	neighborCoord[5].col = currentP.col - 1;
+	neighborCoord[6].row = currentP.row + 1;
+	neighborCoord[6].col = currentP.col;
+	neighborCoord[7].row = currentP.row + 1;
+	neighborCoord[7].col = currentP.col + 1;
+}
+
+int ChainCode::findNextP() {
+	loadNeighborCoord();
+	int sdir = nextQ;
+	do {
+		if(image->zeroFramedAry[neighborCoord[sdir % 8].row][neighborCoord[sdir % 8].col] == 
+			currentCC)
+			break;
+		sdir++;
+	} while ((sdir % 8) != nextQ % 8);
+	return sdir % 8;
+}
+
+void ChainCode::runAlgorithm(ofstream& outfile, ofstream& debugFile) {
+	debugFile << "Starting the Algorithm" << endl;
+	for( currentCC; currentCC <= image->maxVal; currentCC++) {
+		debugFile << "currentCC = " << currentCC << ":\n";
+		minRowOffset = propertyfile[currentCC].properties.minRow;
+		minColOffset = propertyfile[currentCC].properties.minCol;
+		maxRowOffset = propertyfile[currentCC].properties.maxRow;
+		maxColOffset = property[currentCC].properties.maxCol;
+	
+		Point startp;
+		for(int col = minColOffset + 1; col <= maxColOffset + 1; col++) {
+			if(image->zeroFramedAry[minRowOffset + 1][col] == currentCC) {
+				outfile << minRowOffset << " " << col - 1 << " " << currentCC << " ";
+				startp = {minRowOffset, col};
+				debugFile << " startp set to (" << startp.row << "," << startp.col << 
+				" ) ....\n";
+				currentP = { minRowOffset + 1, col};
+				lastQ = 4;
+				do {
+					nextQ = (lastQ + 1) % 8;
+					pChain = findNextP();
+					nextP = neighborCoord[pChain];
+					outfile << pChain << " ";
+					lastQ = nextDirTable[pChain - 1];
+					currentP = nextP;
+					debugFile << "\ncurrentP set to ( " << currentP.row << " ," 
+					<< currentP.col << ") ...\n";
+					} while((currentP.row != startp.row) || (currentP.col != startp.col));
+				outfile << endl;
+				break;
+			} 
+		}
+	}
+		debugFile << "ChainCode code Algorithm execution is now complete\n";
+}
+
+int main(int argc, char* argv[]) {
+	ifstream cc;
+	cc.open(argv[1]);
+	ifstream pp;
+	pp.open(argv[2]);
+	ofstream codefile; 
+	codefile.open(argv[3]);
+	ofstream debugFile;
+	debugFile.open(argv[4]);
+
+	ChainCode chain(cc, pp, codefile, debugFile);
+	chain.runAlgorithm(codefile, debugFile);
+
+	cc.close();
+	pp.close();
+	codefile.close();
+	debugFile.close();
+	return 0;
+}
 
 
 
