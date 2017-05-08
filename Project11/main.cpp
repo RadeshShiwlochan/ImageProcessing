7fb3d45727de64f7adc53fcc301b3f6059c0702b
radeshshiwlochan <radesh0430@gmail.com>
2017-05-08 01:52:36 -0400

    adds ArcChordDistance class with subordinate and helpers functions

--- Project11/main.cpp
+++ Project11/main.cpp
@@ -21,14 +21,13 @@ public:
 	int getIndexVal(int,int);
 	void setIndexVal(int, int, int);
 	void loadPtImage();
-	void prettyPrint(oftream&);	
+	void prettyPrint(ofstream&);	
 };
 
 Image::Image(string inputFile) {
 	ifstream readFile;
 	readFile.open(inputFile);
-	readFile >> numRows >> numCols >> minVal >>
-			 >> maxVal;
+	readFile >> numRows >> numCols >> minVal >> maxVal;
 	imageAry = new int*[numRows];
 	for(int i = 0; i < numCols; ++i)
 		imageAry[i] = new int[numCols]();
@@ -60,9 +59,8 @@ void Image::loadPtImage() {
 
 }
 
-void Image::prettyPrint(printer& ofstream) {
+void Image::prettyPrint(ofstream& printer) {
 
-	printer << line << " cycleCount " << cycleCount <<  endl << endl;
 	for(int row = 0; row < numRows; ++row) {
 		for(int col = 0; col < numCols; ++col) {
 			if(imageAry[row][col] > 0 ) 
@@ -93,12 +91,84 @@ public:
 	void printBoundaryPt();
 };
 
+BoundaryPt::BoundaryPt(int xVal, int yVal, int maxVotesVal, 
+	double maxDistanceVal, int cornerVal)                  {
+	x = xVal;
+	y = yVal;
+	maxVotes = maxVotesVal;
+	maxDistance = maxDistanceVal;
+	corner = cornerVal;
+}
+
+int BoundaryPt::getX() { return x; }
+
+int BoundaryPt::getY() { return y; }
+
+int BoundaryPt::getMaxVotes() { return maxVotes ;}
+
+double BoundaryPt::getMaxDistance() { return maxDistance; }
+
+int BoundaryPt:: getCorner() { return corner; }
+
+void BoundaryPt::printBoundaryPt() {
+	cout << "x: " << x << "\n" << "y: " << y << "\n" << "maxVotes: "
+	<< maxVotes << "\n" << "maxDistance: " << maxDistance << "\n" 
+	<< "corner: " << corner << endl;
+}
+
+class ArcChordDistance {
+private:
+	int kChordLength;
+	int numPts;
+	double* chordAry;
+	int P1;
+	int P2;
+
+public:
+	ArcChordDistance();
+	~ArcChordDistance();
+	void loadData();
+	double computeDistance();
+	int findMaxDist();
+	int computeLocalMaxima();
+	int isCorner(); 	
+};
+
+ArcChordDistance::ArcChordDistance() {
+	kChordLength = 0;
+	numPts = 0;
+	P1 = 0;
+	P2 = 0;
+}
+
+ArcChordDistance::~ArcChordDistance() {
+	//delete [] chordAry;
+}
+
+void ArcChordDistance::loadData() {
+
+}
+
+double ArcChordDistance::computeDistance() {
+	return 0.0;
+}
+
+int ArcChordDistance::findMaxDist() {
+	return 0;
+}
+
+int ArcChordDistance::computeLocalMaxima() {
+	return 0;
+}
+
+int ArcChordDistance::isCorner() { 
+	return 0;
+}
 
 
-
-
-
-
+int main(int argc, char* argv[]) {
+	return 0;
+}
 
 
 
