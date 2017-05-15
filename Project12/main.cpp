7a792061a0325f43977ec2a9da00f7b786d90450
radeshshiwlochan <radesh0430@gmail.com>
2017-05-15 19:40:25 -0400

    fixes segmentation faults

--- Project12/main.cpp
+++ Project12/main.cpp
@@ -9,10 +9,10 @@ private:
 	int numColsIMG;
 	int minValIMG;
 	int maxValIMG;
-  	int numRowsStrctElem;
-	int numColsStrctElem;
-	int minValStrctElem; 
-	int maxValStrctElem;
+  	int nRowsSE;
+	int nColsSE;
+	int minValSE; 
+	int maxValSE;
 	int rowOrigin;
 	int colOrigin;
 	int rowFrameSize;
@@ -33,46 +33,47 @@ public:
 	void closing(int,int);
 	void opening(int,int);   
 	void prettyPrint(int**, int, int, string);
-	void outPutResult(); 
+	void outPutResult();
+	void executeMorphology(); 
 
 };
 
-Morphology::Morphology(string inputImage, string inputStrElem) {
+Morphology::Morphology(string inputImage, string inputSE) {
 
-/**
-	intializes the variables
-*/
 	ifstream readImg;
-	ifstream readStrElem;
+	ifstream readFile;
 	readImg.open(inputImage);
-	readStrElem.open(inputStrElem);
 	readImg >> numRowsIMG >> numColsIMG >> minValIMG >> maxValIMG;
-	readStrElem >> numRowsStrctElem >> numColsStrctElem >> minValStrctElem 
-	            >> maxValStrctElem >> rowOrigin >> colOrigin;
-	 readImg.close();
-	 readStrElem.close();           
+	readImg.close();
+	
+	readFile.open(inputSE);
+
+	readFile >> nRowsSE >> nColsSE >> minValSE >> maxValSE >> rowOrigin >>
+	colOrigin >> rowFrameSize >> colFrameSize;
+	readImg.close();
+          
 }
 
 Morphology::~Morphology() {
 
-	for(int i = 0; i < rowFrameSize; ++i) {
-		delete [] imgAry[i];
-		delete [] morphAry[i];
-	}
-	delete [] imgAry;
-	delete [] morphAry;
+	// for(int i = 0; i < rowFrameSize; ++i) {
+	// 	delete [] imgAry[i];
+	// 	delete [] morphAry[i];
+	// }
+	// delete [] imgAry;
+	// delete [] morphAry;
 
-	for(int i = 0; i < numRowsStrctElem; ++i)
-		delete [] structElemArr[i];
-	delete [] structElemArr;
+	// for(int i = 0; i < nRowsSE; ++i)
+	// 	delete [] structElemArr[i];
+	// delete [] structElemArr;
 }
 /** 
-	rowFrameSize set to numRowsStrctElem (half to the top and half to the bottom) 
-	colFrameSize set to numColsStrctElem (half to the left and half to the right) 
+	rowFrameSize set to nRows (half to the top and half to the bottom) 
+	colFrameSize set to nColsSE (half to the left and half to the right) 
 */
 void Morphology::computeFrameSize() {
-	rowFrameSize = (numRowsStrctElem * 2) + numRowsIMG;
-	colFrameSize = (numColsStrctElem * 2) + numColsIMG;
+	rowFrameSize = (nRowsSE * 2) + numRowsIMG;
+	colFrameSize = (nColsSE * 2) + numColsIMG;
 }
 
 // load imgAry from input1
@@ -80,19 +81,23 @@ void Morphology::loadImage(string inputImage) {
 
 	ifstream readImg;
 	int pixel = -1;
+	computeFrameSize();
 	readImg.open(inputImage);
 	imgAry = new int*[rowFrameSize];
 	for(int i = 0; i < rowFrameSize; ++i)
 		imgAry[i] = new int[colFrameSize]();
 	//skip the header
+	
 	for(int i = 0; i < 3; ++i)
 		readImg >> pixel;
 
-	for(int i = numRowsStrctElem; i < numRowsIMG; ++i) {
-		for(int j = numColsStrctElem; j < numColsIMG; ++j) {
+	for(int i = nRowsSE; i < numRowsIMG + 3; ++i) {
+		for(int j = nColsSE; j < numColsIMG + 3; ++j) {
 			readImg >> pixel;
 			imgAry[i][j] = pixel;
+			cout << pixel << " ";
 		}
+		cout << endl;
 	}//for
 	readImg.close();
 }
@@ -103,15 +108,16 @@ void Morphology::loadstruct(string inputStrElem) {
 	ifstream readStrElem;
 	int pixel = -1;
 	readStrElem.open(inputStrElem);
-	structElemArr = new int*[numRowsStrctElem];
-	for(int i = 0; i < numRowsStrctElem; ++i)
-		structElemArr[i] = new int[numColsStrctElem]();
+	readStrElem >> nRowsSE >> nColsSE >> minValSE >> maxValSE >> rowOrigin >> colOrigin ;
+	structElemArr = new int*[nRowsSE];
+	for(int i = 0; i < nRowsSE; ++i)
+		structElemArr[i] = new int[nColsSE]();
 	//skip the header
-	for(int i = 0; i < 6; ++i)
-		readStrElem >> pixel;
+	// for(int i = 0; i < 6; ++i)
+	// 	readStrElem >> pixel;
 
-	for(int i = 0; i < numRowsStrctElem; ++i) {
-		for(int j = 0; j < numColsStrctElem; ++j) {
+	for(int i = 0; i < nRowsSE; ++i) {
+		for(int j = 0; j < nColsSE; ++j) {
 			readStrElem >> pixel;
 			structElemArr[i][j] = pixel;
 		}
@@ -123,22 +129,22 @@ void Morphology::zeroFrameImage() {
 
 	for(int i = 0; i < rowFrameSize; i++) {
 		imgAry[i][0]              = 0;
-		imgAry[i][numColsIMG + numColsStrctElem - 1] = 0;
+		imgAry[i][numColsIMG + nColsSE - 1] = 0;
 		morphAry[i][0]            = 0;
-		morphAry[i][numColsIMG + numColsStrctElem - 1] = 0;
+		morphAry[i][numColsIMG + nColsSE - 1] = 0;
 
 	}
 
 	for(int j = 0; j < colFrameSize; j++) {
 		 imgAry[0][j]               = 0;
-		 imgAry[numRowsIMG + numRowsStrctElem - 1][j]     = 0;
+		 imgAry[numRowsIMG + nRowsSE - 1][j]     = 0;
 		 morphAry[0][j]             = 0;
-		 morphAry[numRowsIMG + numRowsStrctElem - 1][j]   = 0;
+		 morphAry[numRowsIMG + nRowsSE - 1][j]   = 0;
 	}
 
 }
 
-// at pixel(i,j), i begins at numRowsStrctElem / 2, and j begins at numColsStrctElem / 2
+// at pixel(i,j), i begins at nRow / 2, and j begins at nColsSE / 2
 void Morphology::dilation(int pixel1, int pixel2) {
 
 }
@@ -179,6 +185,12 @@ void Morphology::outPutResult() {
 
 }
 
+void Morphology::executeMorphology() {
+	prettyPrint(imgAry, rowFrameSize, colFrameSize, "image Array");
+	//prettyPrint(morphAry, rowFrameSize, colFrameSize, "morphAry");
+	//prettyPrint(structElemArr, nRowsSE, nColsSE, "structing element");
+}
+
 
 int main(int argc, char* argv[]) {
 	// if(argc != 4) {
@@ -193,10 +205,11 @@ int main(int argc, char* argv[]) {
 	//string outputFile2 = argv[4];
 	//string outputFile3 = argv[5];
 	//string outputFile4 = argv[6]; 
-	Morphology morhology(inputimage, inputStrctElmnt);
+	Morphology morhology(inputimage, argv[2]);
 	morhology.loadImage(inputimage);
-	morhology.loadstruct(inputStrctElmnt);
-	morhology.zeroFrameImage();
+	// //morhology.loadstruct(inputStrctElmnt);
+	//morhology.zeroFrameImage();
+	//morhology.executeMorphology();
 }
 
 
